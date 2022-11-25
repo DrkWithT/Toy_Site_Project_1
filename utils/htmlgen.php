b3cfe5da70b76d4f8393ae855c2658995948364e
DerkT <derektan3510@gmail.com>
2022-11-24 22:52:01 -0800

    Removed uneeded consts.

--- utils/htmlgen.php
+++ utils/htmlgen.php
@@ -6,46 +6,32 @@
    */
   namespace PrintUtils;
 
-  /* Constants */
-  const BEG_LI = "<li>";
-  const END_LI = "</li>";
-  const BEG_POEM_DIV = "<div class=\"work-section\">";
-  const END_DIV = "</div>";
-  const BEG_TITLING = "<h4>";
-  const END_TITLING = "</h4>";
-  const BEG_PAR = "<p class=\"work-content\">";
-  const END_PAR = "</p>";
-
   /* Helper Funcs */
   function printPoemDiv(string $title, string $author, string $text) {
     // open div html
-    $result = BEG_POEM_DIV;
+    $result = "<div class=\"work-section\">";
 
     // append title by PHP template string
-    $result .= BEG_TITLING;
-    $result .= "{$title} by {$author}";
-    $result .= END_TITLING;
+    $result .= "<h4>{$title} by {$author}</h4>";
 
     // append paragraph html
-    $result .= BEG_PAR;
-    $result .= $text;
-    $result .= END_PAR;
+    $result .= "<p class=\"work-content\">{$text}</p>";
 
     // close div html
-    $result .= END_DIV;
+    $result .= "</div>";
 
     return $result;
   }
 
   function printPoemLI(int $poem_id, string $poem_title) {
     // open li html
-    $result = BEG_LI;
+    $result = "<li class=\"work-brief\">";
 
     // append dynamic poem entry data as PHP template string
     $result .= "Poem {$poem_id} is \"{$poem_title}\"";
 
     // close li html
-    $result .= END_LI;
+    $result .= "</li>";
 
     return $result;
   }
